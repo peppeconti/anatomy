cec0c6adf2826e30dad8e85e2701b29915395ff4
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-11-03 14:20:26 +0100

    update

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   const organs = Array.from(document.querySelectorAll(".organ"));
-  console.log(organs);
+  const modal = document.querySelector(".modal");
+  //console.log(organs);
+  modal.addEventListener('click', () => modal.style.display = "none")
   organs.forEach((e) => {
     e.addEventListener("click", (event) => {
-      const modal = document.querySelector(".modal");
+     
       const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
       const copy = e.outerHTML;
-      console.log(copy);
-      console.log(e);
-      svg.width = "100%";
-      svg.height = "100%";
+      //console.log(copy);
+      console.log(e.getBBox());
       svg.innerHTML = copy;
       svg.classList.add('new');
-      console.log(svg)
-      modal.appendChild(svg);
-      modal.style.display = "block";
-      console.log(svg);
+      const copySVG = svg.outerHTML;
+      modal.innerHTML = copySVG;
+      //console.log(svg);
+     
       console.log(modal);
+
+      modal.style.display = "block";
+      const group = modal.querySelector(".organ");
+      const newi = modal.querySelector(".new");
+
+      const factor = (newi.clientHeight*.9)/group.getBBox().height;
+
+      //console.log(factor);
+
+      group.style.transform = `translate(${-group.getBBox().x*factor}px, ${-group.getBBox().y*factor}px) scale(${factor})`
+      console.log(newi.getBBox().height / 100);
+      console.log(newi.clientHeight)
     });
   });
 });
